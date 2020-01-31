670436e73ba5388da193f53f191e19d3387d4635
Demetriex <eujohnmagno@gmail.com>
2020-01-31 11:12:10 +0800

    assets bundle

--- cargame/main.py
+++ cargame/main.py
@@ -1,6 +1,20 @@
-from flask import Flask, render_template, url_for
+import os
+from flask import Flask, render_template
+from flask_assets import Environment, Bundle
 
 app = Flask(__name__)
+assets = Environment(app)
+
+js = Bundle(
+    'js/game.js',
+    'js/background.js',
+    'js/player.js',
+    'js/scenes.js',
+    'js/score.js',
+    'js/spawner.js',
+    output='gen/packed.js'
+)
+assets.register('js_all', js)
 
 
 @app.route("/", methods=["GET"])
